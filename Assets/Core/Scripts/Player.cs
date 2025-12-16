4aa4313520a91ff842110aa1a46ea611ea00cfc0
maxthetypical <greatstar.max@gmail.com>
2025-12-16 13:37:11 +0300

    Coin Collection as Collectables

--- Assets/Core/Scripts/Player.cs
+++ Assets/Core/Scripts/Player.cs
@@ -6,8 +6,11 @@ public class Player : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     [SerializeField] private int _speed = 3;
-    [SerializeField] private int _JumpForce = 10;
+    [SerializeField] private int _JumpForce = 20;
+    [SerializeField] private int _coinsCollectable = 0;
     private PlayerInputAction _playerinputaction = new PlayerInputAction();
+
+    private bool _canjump = false;
 
     private void Start()
     {
@@ -22,39 +25,41 @@ public class Player : MonoBehaviour
 
     private void Jump_performed(InputAction.CallbackContext obj)
     {
-        _rigidbody.AddForce(Vector3.up * _JumpForce); //new Vector3(0, 1, 0)
+        if (_canjump)
+        {
+            _rigidbody.AddForce(Vector3.up * _JumpForce); //new Vector3(0, 1, 0)
+            _canjump = false;
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer==LayerMask.NameToLayer("Floor"))
+        {
+            _canjump = true;
+        }
     }
 
+    private void OnCollisionStay(Collision other)
+    {
+        throw new System.Exception();
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Floor"))
+        {
+            _canjump = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Collectable"))
+            _coinsCollectable++;
+        Destroy(other.gameObject);
+    }
     private void Update()
     {
         Vector2 direction = _playerinputaction.Player.Movement.ReadValue<Vector2>() * _speed;
         _rigidbody.linearVelocity = new Vector3(direction.x, _rigidbody.linearVelocity.y, direction.y);
     }
 }
-//    private void Update()
-//    {
-//        Vector3 direction = Vector3.zero;
-
-//        if (Input.GetKey(KeyCode.W))
-//        {
-//            direction += new Vector3(1, 0, 0) * _speed;
-//        }
-
-//        if (Input.GetKey(KeyCode.S))
-//        {
-//            direction += new Vector3(-1, 0, 0) * _speed;
-//        }
-
-//        if (Input.GetKey(KeyCode.A))
-//        {
-//            direction += new Vector3(0, 0, 1) * _speed;
-//        }
-
-//        if (Input.GetKey(KeyCode.D))
-//        {
-//            direction += new Vector3(0, 0, -1) * _speed;
-//        }
-
-//        _rigidbody.linearVelocity = new Vector3(direction.x, _rigidbody.linearVelocity.y, direction.z);  
-//    }
-//}
